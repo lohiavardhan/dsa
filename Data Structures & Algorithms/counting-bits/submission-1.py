c2e52ca144d036b3cbcc3854120bf40cffa7474d
Vardhan Lohia <65268026+lohiavardhan@users.noreply.github.com>
2026-07-06 16:07:27 +0800

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        memo = [0 for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            memo[i] = memo[i >> 1] + (i & 2)
+        
+        return memo
